12500ef7c127dd8f6d75d1e9def8d7b6ffc227e4
Zach Brown <zacharyc.brown@veteransunited.com>
2026-03-03 21:14:20 -0600

    feat: remove personal laptop

--- flake.nix
+++ flake.nix
@@ -10,20 +10,7 @@
   outputs = inputs@{ self, nix-darwin, nixpkgs }:
   {
     # Build darwin flake using:
-    # $ darwin-rebuild switch --flake .#Zachs-MacBook-Pro
-    darwinConfigurations."Zachs-MacBook-Pro" = nix-darwin.lib.darwinSystem {
-      modules = [
-        ./modules/shared.nix
-        ./modules/personal.nix
-        {
-          system.configurationRevision = self.rev or self.dirtyRev or null;
-          system.stateVersion = 6;
-          system.primaryUser = "zachbrown";
-          nixpkgs.hostPlatform = "aarch64-darwin";
-        }
-      ];
-    };
-
+    # $ darwin-rebuild switch --flake .#VU-D4RW65L6QG
     darwinConfigurations."VU-D4RW65L6QG" = nix-darwin.lib.darwinSystem {
       modules = [
         ./modules/shared.nix
